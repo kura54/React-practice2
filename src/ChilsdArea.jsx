1d8b6f23eea5e66e1e7cc57ade09c705c63b6faf
kura <91504106+kura54@users.noreply.github.com>
2023-11-15 07:53:17 +0000

    Update App.js and ChilsdArea.jsx

--- src/ChilsdArea.jsx
+++ src/ChilsdArea.jsx
@@ -1,18 +1,28 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor: "khaki",
 };
 
-export const ChilsdArea = (props) => {
-  const { open } = props;
+export const ChilsdArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log("ChildAreaがレンタリングされた");
+
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+
   return (
     <>
       {open ? (
         <div style={style}>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
   );
-};
+});
